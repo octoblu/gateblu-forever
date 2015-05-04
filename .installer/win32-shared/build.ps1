52ee7e5077fd821ac4418bb736b50806ed0ac00c
Jade Meskill <jade.meskill@gmail.com>
2015-05-03 21:04:22 -0700

    add bin

--- .installer/win32-shared/build.ps1
+++ .installer/win32-shared/build.ps1
@@ -25,7 +25,7 @@ $output_dir = "$script_dir\output"
 $cache_dir = "$script_dir\cache"
 $tmp_dir = [io.path]::GetTempFileName()
 $wix_template_dir = "$shared_dir\wix"
-$wix_dir = "C:\Program Files (x86)\WiX Toolset v3.8"
+$wix_dir = "C:\Program Files (x86)\WiX Toolset v3.8\bin"
 
 @(
     $output_dir
